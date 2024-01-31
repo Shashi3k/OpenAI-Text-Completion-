acc837d353a44b3d067572cfbfba430d9eb53cb5
Shashi <139968956+Shashi3k@users.noreply.github.com>
2024-01-31 23:39:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const openAI_secret_key="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"
+
+async function fetchData(){
+    const response = await fetch("https://api.openai.com/v1/completions",{
+        method: "POST",
+        headers:{
+            Authorization: "Bearer " + openAI_secret_key,
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            model:"gpt-3.5-turbo-instruct",
+            prompt:"hello there!",
+            max_tokens:7,
+            temperature:0.5,
+            top_p:1.0,
+            frequency_penalty:0.0,
+            presence_penalty:0.0,
+            stop:""
+    })
+
+})
+    const data = await response.json()
+    console.log(data)
+}
+
+fetchData()
