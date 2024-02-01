08c6e158fc1947d86792832f480e941a66fcabb9
Shashi <139968956+Shashi3k@users.noreply.github.com>
2024-02-01 09:27:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const openAI_secret_key=""
 
 async function fetchData(){
     const response = await fetch("https://api.openai.com/v1/completions",{
+        /**You can also use https://api.openai.com/v1/chat/completions to complete your chats. When 
+        you do that, remember to use model gpt-3.5-turbo-1160. You can also
+        use gpt-4 model for higher performance but unfortunately, that api key is not free.
+        You can learn more about it from here: https://help.openai.com/en/articles/7102672-how-can-i-access-gpt-4
+        */
         method: "POST",
         headers:{
             Authorization: "Bearer " + openAI_secret_key,
